04dd8f9e44f6dbfd41d7623a9ff00cfd79990559
Mehran Kasebvatan <mehran.kasebvatan@gmail.com>
2024-05-10 18:48:56 +0330

    home app almost completed! just content must be edit.

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, ResumePage, PortfolioPage, ContactPage
 
+app_name = 'home'
 urlpatterns = [
     path("", HomePage.as_view(), name="home_page"),
+    path("resume/", ResumePage.as_view(), name="resume_page"),
+    path("portfolio/", PortfolioPage.as_view(), name="portfolio_page"),
+    path("contact/", ContactPage.as_view(), name="contact_page"),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,15 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePage(TemplateView):
     template_name = "home/index.html"
+
+
+class ContactPage(TemplateView):
+    template_name = "home/contact.html"
+
+
+class ResumePage(TemplateView):
+    template_name = "home/resume.html"
+
+
+class PortfolioPage(TemplateView):
+    template_name = "home/portfolio.html"
